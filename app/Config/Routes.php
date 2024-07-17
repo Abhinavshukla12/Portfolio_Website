2f64c0764f2aacd5674c3aa6646073c866ba1ccb
Abhinav shukla <124790218+Abhinavshukla12@users.noreply.github.com>
2024-07-17 16:21:33 +0530

    making advanced blog so remove all blog related file code

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,9 +5,4 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'HomeController::index');
-$routes->get('about', 'HomeController::about');
-$routes->get('projects', 'ProjectController::index');
-$routes->get('blog', 'BlogController::index');
-$routes->get('/contact', 'HomeController::contact');
-$routes->post('/contact', 'HomeController::sendContact');
+$routes->get('/', 'Home::index');
--- public/js/script.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('JavaScript loaded');
-    // Add any interactive features or DOM manipulations here
-});
