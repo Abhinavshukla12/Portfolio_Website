03face00c097cb2711edc84ec4fa4e323442afbe
Abhinav shukla <124790218+Abhinavshukla12@users.noreply.github.com>
2024-07-18 20:06:57 +0530

    delete unwanted files

--- app/Routes/ProjectRoutes.php
+++ app/Routes/ProjectRoutes.php
@@ -8,28 +8,8 @@ $routes = Services::routes();
 $routes->group('project/', ['namespace' => 'App\Controllers'], static function ($routes) {
     //home page route
     $routes->get('home', 'DashboardController::index');
+    $routes->get('blog', 'BlogController::index');
+    $routes->get('blog/view/(:segment)', 'BlogController::view/$1');
 
-    $routes->get('/', 'PostController::index');
-    $routes->get('post/create', 'PostController::create');
-    $routes->get('post/edit/(:segment)', 'PostController::edit/$1');
-    $routes->get('post/view/(:segment)', 'PostController::view/$1');
-    $routes->get('post/delete/(:segment)', 'PostController::delete/$1');
-
-    $routes->get('user/register', 'UserController::register');
-    $routes->get('user/login', 'UserController::login');
-    $routes->get('user/profile/(:segment)', 'UserController::profile/$1');
-
-    $routes->get('comment/add/(:segment)', 'CommentController::add/$1');
-    $routes->get('comment/list/(:segment)', 'CommentController::list/$1');
-
-    $routes->get('category', 'CategoryController::index');
-    $routes->get('category/create', 'CategoryController::create');
-    $routes->get('category/edit/(:segment)', 'CategoryController::edit/$1');
-    $routes->get('category/delete/(:segment)', 'CategoryController::delete/$1');
-
-    $routes->get('tag', 'TagController::index');
-    $routes->get('tag/create', 'TagController::create');
-    $routes->get('tag/edit/(:segment)', 'TagController::edit/$1');  
-    $routes->get('tag/delete/(:segment)', 'TagController::delete/$1');
 });
 //routes end
